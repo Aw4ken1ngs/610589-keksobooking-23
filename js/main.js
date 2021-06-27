f674cc327f4a831f7ed1599f5b04a12d18b6a578
Ivan <Aw4ken1ngs@gmail.com>
2021-06-27 22:01:49 +0300

    больше деталей

--- js/main.js
+++ js/main.js
@@ -1,29 +1,83 @@
-const getRandom = function (min, max) {
-  if (max < min) {
-    const temp = max;
-    max = min;
-    min = temp;
-  }
+import { getRandomInteger, getRandomFloat } from './utils.js';
 
-  if (max === min) {
-    return max;
-  }
+const TITLE = ['Мини-отель Аквамарин', 'Солдайя Гранд Отель', 'Гостиница Polli'];
+const TYPE = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const CHECKIN = ['12:00', '13:00', '14:00'];
+const CHECKOUT = ['12:00', '13:00', '14:00'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const DESCRIPTION = ['Oтель находится в Судаке, всего в 10 минутах ходьбы от моря', 'с панорамным видом на город', 'К услугам гостей крытый бассейн и современный спа-центр', 'расположен в тихом месте в центре города'];
+const PHOTOS = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
 
-  return min + Math.random() * (max + 1 - min);
+const priceRange = {
+  min: 1000,
+  max: 10000,
 };
 
-function getRandomInteger(min, max) {
-  const randomNumber = getRandom(min, max);
-
-  return Math.floor(randomNumber);
+const idPhotoRange = {
+  min: 1,
+  max: 8,
 }
 
-getRandomInteger(3, 10);
+const roomsRange = {
+  min: 1,
+  max: 7,
+};
 
-function getRandomFloat(min, max, simbolsAfterComma) {
-  const randomNumber = getRandom(min, max);
+const guestsRange = {
+  min: 1,
+  max: 3,
+};
 
-  return randomNumber.toFixed(simbolsAfterComma);
-}
+const lat = {
+  min: 35.65000,
+  max: 35.70000,
+};
 
-getRandomFloat(5.78, 10.495, 2);
+const lng = {
+  min: 139.7,
+  max: 139.8,
+};
+
+const arrayLength = 10;
+
+const getRandomArrayElement = (elements) => {
+  return elements[getRandomInteger(0, elements.length - 1)];
+};
+
+const getRandomFeatures = () => {
+  return FEATURES.filter(() => {
+    return getRandomInteger(0, 1);
+  })
+};
+
+const twoDigitNumber = () => {
+  if (getRandomInteger < 10)
+    return "0" + getRandomInteger;
+  return getRandomInteger;
+};
+
+const offer = () => {
+  return {
+    author: {
+      avatar: 'img/avatars/user0' + twoDigitNumber(idPhotoRange.min, idPhotoRange.max) + '.png',
+    },
+    offer: {
+      title: getRandomArrayElement(TITLE),
+      address: getRandomFloat(idPhotoRange.min, idPhotoRange.max) + ', ' + getRandomFloat(lat.min, lng.max),
+      price: getRandomInteger(priceRange.min, priceRange.max),
+      type: getRandomArrayElement(TYPE),
+      rooms: getRandomInteger(roomsRange.min, roomsRange.max),
+      guests: getRandomInteger(guestsRange.min, guestsRange.max),
+      checkin: getRandomArrayElement(CHECKIN),
+      checkout: getRandomArrayElement(CHECKOUT),
+      features: getRandomFeatures(),
+      description: getRandomArrayElement(DESCRIPTION),
+      photos: getRandomArrayElement(PHOTOS),
+    },
+
+    location: {
+      X: getRandomNumber(lat.min, lat.max),
+      Y: getRandomNumber(lng.min, lng.max),
+    },
+  },
+};
